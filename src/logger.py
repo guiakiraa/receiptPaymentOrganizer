4cd2bcc390fa4d7b8b4655da01970f3a66c0493f
guiakiraa <guilhermeakira0810@gmail.com>
2026-04-28 22:34:27 -0300

    feat: add logger

---
+++ src/logger.py
@@ -0,0 +1,22 @@
+import logging
+import sys
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        fmt="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    return logger
